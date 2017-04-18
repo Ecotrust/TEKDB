cbfa44c8f3ca32c38c7ebcfb5654752a86c68a3f
mitt4git <navifyconsulting@gmail.com>
2017-04-18 14:53:06 -0700

    URL additon and templates
    few front end changes

--- code/TEKDB/TEKDB/urls.py
+++ code/TEKDB/TEKDB/urls.py
@@ -13,10 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+#from django.conf.urls import url
+#from django.contrib import admin
+
+#urlpatterns = [
+#    url(r'^admin/', admin.site.urls),
+#	url (r'^$', views.index, name='index'),
+#]
+
+#from django.conf import settings
+#from django.conf.urls.static import static
+
+from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+	url(r'^login/', include('login.urls')),
     url(r'^admin/', admin.site.urls),
-	url (r'^$', views.index, name='index'),
 ]
+
+#if settings.DEBUG:
+#    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
