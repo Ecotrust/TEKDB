18e7194d870f55b6d8908405f3721bd3f0c4c5f7
Ryan Hodges <ryan.d.hodges@gmail.com>
2017-05-31 19:36:16 -0700

    updating settings for heroku :

--- TEKDB/settings.py
+++ TEKDB/settings.py
@@ -135,10 +135,10 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-#STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, "static"),
-#    '/var/www/static/',
-#]
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, "static"),
+   '/var/www/static/',
+]
 
 ### DJANGO-REGISTRATION SETTINGS ###
 REGISTRATION_OPEN = True
