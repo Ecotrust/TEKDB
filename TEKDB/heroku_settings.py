7465404bd1db2dec34a133b18112f699dc6356d9
Ryan Hodges <ryan.d.hodges@gmail.com>
2017-06-01 10:40:46 -0700

    attempt heroku with postgres and py3

--- TEKDB/heroku_settings.py
+++ TEKDB/heroku_settings.py
@@ -1,9 +1,18 @@
 import os
 REGISTRATION_OPEN = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgresql_psycopg2',
+        'NAME': 'tekdb',
+        'USER': 'postgres',
     }
 }
