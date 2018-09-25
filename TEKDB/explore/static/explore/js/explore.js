3e4d5c805429fd038b52fc4c48688977e99d7abb
Peregrine Edison-Lahm <peregrine_env@comcast.net>
2018-09-25 14:24:37 -0700

    Fix script to select search filter and highlight selected search filter button

--- TEKDB/explore/static/explore/js/explore.js
+++ TEKDB/explore/static/explore/js/explore.js
@@ -1,7 +1,5 @@
-<script>
   function setCategoryType(category) {
     $('#search-category-type')[0].value = category;
     $('.selected').removeClass('selected');
     $('#category-btn-'+category).addClass('selected');
   }
-</script>
