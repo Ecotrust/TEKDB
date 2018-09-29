ff54b1fc8d88e686cf5c2f5ad0947726e4771cc5
Peregrine Edison-Lahm <peregrine_env@comcast.net>
2018-09-28 17:01:02 -0700

    Fixes issue #82.The Explore page now highlights the correct button on reload.

--- TEKDB/explore/static/explore/js/explore.js
+++ TEKDB/explore/static/explore/js/explore.js
@@ -1,5 +1,27 @@
-  function setCategoryType(category) {
-    $('#search-category-type')[0].value = category;
+$(document).ready(function() {
+	setFilter();
+	eqHeightBtns();
+});
+
+function setFilter() {
+ 	var category = $('#search-category-type')[0].value;
+ 	$('.btn-default').removeClass('selected');
+ 	$('#category-btn-'+category).addClass('selected');
+}
+
+function eqHeightBtns() {
+    var highestBox = 0;
+    $('.btn-group-justified .btn').each(function(){  
+        if($(this).height() > highestBox){  
+            highestBox = $(this).height();  
+        }
+    	});    
+    $('.btn-group-justified .btn').height(highestBox);
+}
+
+/*This function runs when the users clicks the Search button.*/
+function setCategoryType(category) {
+	$('#search-category-type')[0].value = category;
     $('.selected').removeClass('selected');
     $('#category-btn-'+category).addClass('selected');
-  }
+}
