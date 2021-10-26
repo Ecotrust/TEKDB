4113732afebf74114d322acd376a0b5e21dc0c8f
Ryan Hodges <ryan.d.hodges@gmail.com>
2021-10-26 08:48:33 -0700

    switch to postgis docker image

--- TEKDB/TEKDB/settings.py
+++ TEKDB/TEKDB/settings.py
@@ -101,9 +101,15 @@ WSGI_APPLICATION = 'TEKDB.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'tekdb',
-        'USER': 'postgres',
+        # 'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        # 'NAME': 'tekdb',
+        # 'USER': 'postgres',
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR,"db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
 
