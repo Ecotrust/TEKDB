33f04bbea77f0ec6a968f00f2aca2a956e9cbf03
Ryan Hodges <ryan.d.hodges@gmail.com>
2021-10-26 16:39:25 -0700

    updating deployment to support fresh, non-vagrant launches

--- TEKDB/docker/entrypoint.sh
+++ TEKDB/docker/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+ln -s /usr/local/apps/TEKDB/TEKDB/docker/local_settings.py /usr/local/apps/TEKDB/TEKDB/TEKDB/local_settings.py
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 python manage.py runserver 0.0.0.0:8000
