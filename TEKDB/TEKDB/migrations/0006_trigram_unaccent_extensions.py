c4a82bb102fb8e49f7eaa35cb39625c71f2f4ea3
Ryan Hodges <ryan.d.hodges@gmail.com>
2022-01-06 13:58:55 -0800

    add trigram migration

---
+++ TEKDB/TEKDB/migrations/0006_trigram_unaccent_extensions.py
@@ -0,0 +1,13 @@
+from django.contrib.postgres.operations import TrigramExtension, UnaccentExtension
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+
+  dependencies = [
+      ('TEKDB', '0005_auto_20211021_1607'),
+  ]
+
+  operations = [
+    TrigramExtension(),
+    UnaccentExtension()
+  ]
