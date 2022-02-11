312181bb29f0e16e22ea1580f58d2746e5e68fa1
Ryan Hodges <ryan.d.hodges@gmail.com>
2022-02-11 14:18:53 -0800

    adding script to set Windows environment variables for GDAL

---
+++ Windows/set_osgeo_environment.bat
@@ -0,0 +1,7 @@
+set OSGEO4W_ROOT=C:\OSGeo4W
+set GDAL_DATA=%OSGEO4W_ROOT%\share\gdal
+set PROJ_LIB=%OSGEO4W_ROOT%\share\proj
+set PATH=%PATH%;%OSGEO4W_ROOT%\bin
+reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v Path /t REG_EXPAND_SZ /f /d "%PATH%"
+reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v GDAL_DATA /t REG_EXPAND_SZ /f /d "%GDAL_DATA%"
+reg ADD "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v PROJ_LIB /t REG_EXPAND_SZ /f /d "%PROJ_LIB%"
