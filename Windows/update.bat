8efb797afc5f4750da09bb04db0c1a2795cf3e97
Ryan Hodges <ryan.d.hodges@gmail.com>
2022-04-08 09:58:59 -0700

    Tweaks to windows RUS update script

--- Windows/update.bat
+++ Windows/update.bat
@@ -6,8 +6,9 @@ cd %PROJ_ROOT%
 
 setlocal
 :: RUS? Maybe Backup first?
-echo "It is recommended you export a backup of your data prior to running updates."
-echo "Common approaches include using the built-in 'export' tool in the admin console or taking a snapshot of the server state (ideally both)."
+echo WARNING:
+echo It is recommended you export a backup of your data prior to running updates.
+echo Common approaches include using the built-in 'export' tool in the admin console or taking a snapshot of the server state (ideally both).
 :PROMPT
 SET /P AREYOUSURE=Have you backed up your data and prepared to go through with this update (Y/[N])?
 IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
