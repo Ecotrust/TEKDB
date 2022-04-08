264acb188a36a0c4fdcc0719015c7cc5d048a710
Ryan Hodges <ryan.d.hodges@gmail.com>
2022-04-07 17:35:55 -0700

    smarter faster windows update batch script

--- Windows/update.bat
+++ Windows/update.bat
@@ -1,10 +1,11 @@
-set PROJ_ROOT=C:\Apps\TEKDB
+@echo off
+set CWD=%~dp0
+set PROJ_ROOT=%CWD%..
 set PYTHON=%PROJ_ROOT%\env\Scripts\python.exe
 cd %PROJ_ROOT%
 git pull
 %PYTHON% -m pip install -r %PROJ_ROOT%\TEKDB\requirements.txt
 %PYTHON% %PROJ_ROOT%\TEKDB\manage.py migrate
-%PYTHON% %PROJ_ROOT%\TEKDB\manage.py collectstatic
+%PYTHON% %PROJ_ROOT%\TEKDB\manage.py collectstatic --no-input
 %PYTHON% %PROJ_ROOT%\TEKDB\manage.py compress
 C:\Windows\System32\iisreset.exe
-PAUSE
