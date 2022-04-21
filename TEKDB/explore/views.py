37c1d095476200d8d513d8fd81e789060dc19041
David Pollard <dpollard@ecotrust.org>
2022-04-21 16:50:41 -0700

    Add back compass icons

--- TEKDB/explore/views.py
+++ TEKDB/explore/views.py
@@ -41,13 +41,13 @@ def get_proj_css():
 
 def get_proj_icons():
     proj_icons = {
-    'logo': 'explore/img/logos/ITK_lines_logo.png',
-    'place_icon': 'explore/img/record_icons/place_icon.png',
-    'resource_icon': 'explore/img/record_icons/resource_icon.png',
-    'activity_icon': 'explore/img/record_icons/activity_icon.png',
-    'source_icon': 'explore/img/record_icons/source_icon.png',
-    'media_icon': 'explore/img/record_icons/media_icon.png',
-}
+        'logo': 'explore/img/logos/ITK_lines_logo.png',
+        'place_icon': 'explore/img/record_icons/place_icon.png',
+        'resource_icon': 'explore/img/record_icons/resource_icon.png',
+        'activity_icon': 'explore/img/record_icons/activity_icon.png',
+        'source_icon': 'explore/img/record_icons/source_icon.png',
+        'media_icon': 'explore/img/record_icons/media_icon.png',
+    }
     try:
         from TEKDB.settings import PROJ_ICONS
         if 'logo' in PROJ_ICONS.keys():
