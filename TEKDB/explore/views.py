08c5ebb9352a4e42f5e020753bb381a8680752df
David Pollard <dpollard@ecotrust.org>
2022-04-21 16:53:24 -0700

    Update path to proj_icons fallbacks in views

--- TEKDB/explore/views.py
+++ TEKDB/explore/views.py
@@ -41,12 +41,12 @@ def get_proj_css():
 
 def get_proj_icons():
     proj_icons = {
-        'logo': 'explore/img/logos/ITK_lines_logo.png',
-        'place_icon': 'explore/img/record_icons/place_icon.png',
-        'resource_icon': 'explore/img/record_icons/resource_icon.png',
-        'activity_icon': 'explore/img/record_icons/activity_icon.png',
-        'source_icon': 'explore/img/record_icons/source_icon.png',
-        'media_icon': 'explore/img/record_icons/media_icon.png',
+        'logo': 'explore/img/logos/logo_weave.svg',
+        'place_icon': 'explore/img/icons/i_place.svg',
+        'resource_icon': 'explore/img/icons/i_resource.svg',
+        'activity_icon': 'explore/img/icons/i_activity.svg',
+        'source_icon': 'explore/img/icons/i_source.svg',
+        'media_icon': 'explore/img/icons/i_media.svg',
     }
     try:
         from TEKDB.settings import PROJ_ICONS
