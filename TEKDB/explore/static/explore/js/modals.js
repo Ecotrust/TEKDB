2ae5ae6bb17c8eae9cbefcabaaff786b93f8a6cf
David Pollard <dpollard@ecotrust.org>
2022-06-14 16:47:18 -0700

    Styles for error messages began. Switching branches

--- TEKDB/explore/static/explore/js/modals.js
+++ TEKDB/explore/static/explore/js/modals.js
@@ -1,3 +1,13 @@
 $('#loginModal').on('shown.bs.modal', function () {
   $('#loginInput').focus()
+  $(this).find('input[type="submit"]').on('submit', function(event) {
+    event.preventDefault();
+    /** 
+     * 1. Write a test for this
+     * then code:
+     * 2. Make login AJAX request
+     * 3. Added logic for better error message
+     * 4. ...
+     * */
+  });
 })
