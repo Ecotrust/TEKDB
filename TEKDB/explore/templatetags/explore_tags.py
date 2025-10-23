4ffb0d0f3be5585ad8348615e919634c40091789
Paige Williams <pwilliams@ecotrust.org>
2025-10-22 17:15:04 -0700

    ruff format

--- TEKDB/explore/templatetags/explore_tags.py
+++ TEKDB/explore/templatetags/explore_tags.py
@@ -11,7 +11,7 @@ def mult(value, arg):
 
 # redefining 'mult' from line 6, should this be 'subtract'?
 @register.filter(name="subtract")
-def mult(value, arg): # noqa: F811 
+def mult(value, arg):  # noqa: F811
     """Subtracts the arg from the value"""
     return int(value) - int(arg)
 
