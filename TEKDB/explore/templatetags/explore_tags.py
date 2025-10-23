cf021dd018e4ecaa4fa0dae9e287e034096e1d9f
Paige Williams <pwilliams@ecotrust.org>
2025-10-22 17:06:19 -0700

    add noqa: F811 and comment

--- TEKDB/explore/templatetags/explore_tags.py
+++ TEKDB/explore/templatetags/explore_tags.py
@@ -9,8 +9,9 @@ def mult(value, arg):
     return int(value) * int(arg)
 
 
+# redefining 'mult' from line 6, should this be 'subtract'?
 @register.filter(name="subtract")
-def mult(value, arg):
+def mult(value, arg): # noqa: F811 
     """Subtracts the arg from the value"""
     return int(value) - int(arg)
 
