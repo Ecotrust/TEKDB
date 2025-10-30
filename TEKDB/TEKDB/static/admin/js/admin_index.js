ee7ffddd77329a08e0597c096de5cf2bb215b754
Paige Williams <pwilliams@ecotrust.org>
2025-10-30 15:16:20 -0700

    use bootstrap modal to display info content

--- TEKDB/TEKDB/static/admin/js/admin_index.js
+++ TEKDB/TEKDB/static/admin/js/admin_index.js
@@ -44,64 +44,55 @@ $(function() {
     });
 
     $('button#export-info').click(function(e) {
-      window.alert(
-        "Export Database Tool:\n\n" +
-
+      $('#modalTitle').text("Export Database Tool");
+      $('#modalBody').html(
         "The Export Database Tool is designed to support saving the current " +
         "state of your Traditional Knowledge Database. This is best " +
-        "used in conjunction with the 'Import Database' tool below. \n\n" +
+        "used in conjunction with the 'Import Database' tool below. </br><br>" +
 
         "To use the Export Database tool is simple: just press 'Export to " +
         ".zip' button and all of your data and media files will be collected " +
-        "into a single zipfile and downloaded to your current computer." +
-        "\n\n" +
+        "into a single zipfile and downloaded to your current computer.</br></br>" +
 
         "This is VERY DANGEROUS! While keeping regular backups " +
         "of your database is crucial to preventing data loss from future " +
         "incidents, the exported file will contain ALL of your data in an  " +
         "unencrypted format. These exported files  " +
         "must be kept in a secure place and only shared via secure channels: " +
-        "NOT BY EMAIL.\n\n" +
+        "NOT BY EMAIL.</br></br>" +
 
         "Please work with IT professionals to design " +
         "a safe and secure practice for regular backups, which may or may " +
         "not involve this tool."
       );
-    })
+    });
 
     $('button#import-info').click(function(e) {
-      window.alert(
-        "Import Database Tool:\n\n" +
-
-        "The Import Database Tool is designed to support restoring your " +
-        "Traditional Knowledge Database back to a prior state. This is best " +
-        "used in conjunction with the 'Export Database' tool above. \n\n" +
-
-        "To use the Import Database tool is simple: select a properly formatted " +
-        "zip file to upload and then click 'Import'. All of your data will " +
-        "be reverted back to the state it was when that zipfile was created." +
-        "\n\n" +
+      $('#modalTitle').text("Import Database Tool");
+      $('#modalBody').html(
+        "The Import Database Tool is designed to support restoring your Traditional Knowledge Database back to a prior state. This is best used in conjunction with the 'Export Database' tool above. </br><br>" +
+        
+        "To use the Import Database tool is simple: select a properly formatted zip file to upload and then click 'Import'. All of your data will be reverted back to the state it was when that zipfile was created. </br><br>" +
 
         "This is VERY DANGEROUS! For this to work, all of the data currently " +
         "in your database, including your users, your records, and your page " +
         "contents will be removed, and then replaced. Also, any files " +
         "associated with your media records (images, audio, video, PDFs, " +
         "etc...) will be overwritten by any files of the same name included " +
-        "in the zipped up backup file that you import.\n\n" +
+        "in the zipped up backup file that you import.</br><br>" +
 
         "If you wish to use this, it is recommended that you work with IT " +
         "professionals prior to undertaking this task. While IT cannot create " +
         "one of these import files for a prior state, they can help you " +
         "securely preserve any new zipped export files, and should be able " +
         "to create a system backup of your database to restore in the event " +
-        "of complications while using this tool. \n\n" +
+        "of complications while using this tool. </br><br>" +
 
         "Note that a zipped backup file is easily created for your current " +
         "state with the 'Export to .zip' button above, but cannot be " +
         "created by hand. If you have not created any of these files, you " +
-        "should not use this tool."
-      );
-    })
+        "should not use this tool.");
+    });
 
 
 
