c8ed8c2c85f8c44b09ac131241733894b9491b5c
Paige Williams <pwilliams@ecotrust.org>
2025-12-01 15:42:47 -0800

    add pagination to bottom of results page

--- TEKDB/explore/static/explore/js/list_view.js
+++ TEKDB/explore/static/explore/js/list_view.js
@@ -1,23 +1,23 @@
 // BIG MESSY STATE MANAGEMENT //
 
-var reset_triggers = function() {
-    setTimeout(function() {
-      //Change Page
-      $('.paginate_button').off('click');
-      $('.paginate_button').click(page_click);
-    }, 20);
-}
+var reset_triggers = function () {
+  setTimeout(function () {
+    //Change Page
+    $(".paginate_button").off("click");
+    $(".paginate_button").click(page_click);
+  }, 20);
+};
 
-var page_click = function(event){
+var page_click = function (event) {
   page = event.target.text;
   if (isNaN(page)) {
     current_page = parseInt(app.resultViewModel.state_page());
     var max_page = app.datatable.page.info().pages;
     var min_page = 1;
-    if (page == '«') {
-      page = current_page-1;
-    } else if (page == '»') {
-      page = current_page+1;
+    if (page == "«") {
+      page = current_page - 1;
+    } else if (page == "»") {
+      page = current_page + 1;
     } else {
       page = current_page;
     }
@@ -28,110 +28,132 @@ var page_click = function(event){
   if (page > max_page) {
     page = max_page;
   }
-  window.location.hash = $.query.SET('page', page);
+  window.location.hash = $.query.SET("page", page);
   app.resultViewModel.state_page(parseInt(page));
   reset_triggers();
-}
+};
 
-var get_current_page = function() {
-  return parseInt($('.paginate_button.current').text());
-}
+var get_current_page = function () {
+  return parseInt($(".paginate_button.current").text());
+};
 
-var change_items_per_page = function(event) {
+var change_items_per_page = function (event) {
   items_per_page = parseInt(event.target.value);
   app.resultViewModel.state_items_per_page(items_per_page);
   app.resultViewModel.state_page(get_current_page());
-  window.location.hash = $.query.SET('items_per_page', items_per_page).SET('page', app.resultViewModel.state_page());
+  window.location.hash = $.query
+    .SET("items_per_page", items_per_page)
+    .SET("page", app.resultViewModel.state_page());
   reset_triggers();
-}
+};
 
-var change_filter = function(event) {
-  setTimeout(function() {
+var change_filter = function (event) {
+  setTimeout(function () {
     filter = event.target.value;
     app.resultViewModel.state_page(get_current_page());
-    window.location.hash = $.query.SET('page', app.resultViewModel.state_page());
+    window.location.hash = $.query.SET(
+      "page",
+      app.resultViewModel.state_page()
+    );
     if (filter.length > 0) {
       app.resultViewModel.db_query(filter);
-      window.location.hash = $.query.SET('filter', app.resultViewModel.db_query());
+      window.location.hash = $.query.SET(
+        "filter",
+        app.resultViewModel.db_query()
+      );
     } else {
-      app.resultViewModel.db_query('*');
-      window.location.hash = $.query.REMOVE('filter');
+      app.resultViewModel.db_query("*");
+      window.location.hash = $.query.REMOVE("filter");
     }
     reset_triggers();
   }, 10);
-}
+};
 
-var change_sort = function(event) {
+var change_sort = function (event) {
   order = [];
-  column_keys = ['name', 'category', 'description'];
-  for (var i = 0; i < column_keys.length; i++){
-    sort_class = $("#"+ column_keys[i]+"_col").attr('class');
+  column_keys = ["name", "category", "description"];
+  for (var i = 0; i < column_keys.length; i++) {
+    sort_class = $("#" + column_keys[i] + "_col").attr("class");
     console.log(sort_class);
-    if (sort_class == 'sorting_desc') {
-      order.push([i+1, 'desc']);
-    } else if (sort_class == 'sorting_asc') {
-      order.push([i+1, 'asc']);
+    if (sort_class == "sorting_desc") {
+      order.push([i + 1, "desc"]);
+    } else if (sort_class == "sorting_asc") {
+      order.push([i + 1, "asc"]);
     }
   }
   app.resultViewModel.state_order(order);
   app.resultViewModel.state_page(get_current_page());
-  window.location.hash = $.query.SET('page', app.resultViewModel.state_page());
+  window.location.hash = $.query.SET("page", app.resultViewModel.state_page());
   if (app.resultViewModel.state_order().length > 0) {
-    window.location.hash = $.query.SET('order', app.resultViewModel.state_order());
+    window.location.hash = $.query.SET(
+      "order",
+      app.resultViewModel.state_order()
+    );
   } else {
-    window.location.hash = $.query.REMOVE('order');
+    window.location.hash = $.query.REMOVE("order");
   }
   reset_triggers();
+};
 
-}
-
-$(document).ready( function () {
-  if ($.query.get('page') != "") {
-    app.resultViewModel.state_page($.query.get('page'));
+$(document).ready(function () {
+  if ($.query.get("page") != "") {
+    app.resultViewModel.state_page($.query.get("page"));
   }
-  if ($.query.get('items_per_page') != "") {
-    app.resultViewModel.state_items_per_page(parseInt($.query.get('items_per_page')));
+  if ($.query.get("items_per_page") != "") {
+    app.resultViewModel.state_items_per_page(
+      parseInt($.query.get("items_per_page"))
+    );
   }
-  if ($.query.get('filter') != "" && $.query.get('filter') != true) {
-    app.resultViewModel.db_query($.query.get('filter'));
+  if ($.query.get("filter") != "" && $.query.get("filter") != true) {
+    app.resultViewModel.db_query($.query.get("filter"));
   }
-  if ($.query.get('order') != "") {
-    app.resultViewModel.state_order($.query.get('order'));
+  if ($.query.get("order") != "") {
+    app.resultViewModel.state_order($.query.get("order"));
   }
 
   var results_per_page_options = [10, 25, 50, 100];
-  if (results_per_page_options.indexOf(app.resultViewModel.state_items_per_page()) < 0) {
+  if (
+    results_per_page_options.indexOf(
+      app.resultViewModel.state_items_per_page()
+    ) < 0
+  ) {
     results_per_page_options.push(app.resultViewModel.state_items_per_page());
   }
-  results_per_page_options.sort(function(a, b){return a - b});
+  results_per_page_options.sort(function (a, b) {
+    return a - b;
+  });
   /* displayStart takes the index of the record to show, not the page, 0-addressed */
-  var display_start = (app.resultViewModel.state_page() - 1)*app.resultViewModel.state_items_per_page();
-  var initial_filter = app.resultViewModel.db_query() == '*' ? '' : app.resultViewModel.db_query();
+  var display_start =
+    (app.resultViewModel.state_page() - 1) *
+    app.resultViewModel.state_items_per_page();
+  var initial_filter =
+    app.resultViewModel.db_query() == "*" ? "" : app.resultViewModel.db_query();
 
-  app.datatable = $('#results_table').DataTable({
-    "displayStart": display_start,
-    "pageLength": app.resultViewModel.state_items_per_page(),
-    "lengthMenu": results_per_page_options,
+  app.datatable = $("#results_table").DataTable({
+    displayStart: display_start,
+    pageLength: app.resultViewModel.state_items_per_page(),
+    lengthMenu: results_per_page_options,
     // "search": {
     //   "search": initial_filter
     // },
-    "order": app.resultViewModel.state_order(),
-    "columnDefs": [
-      { "orderable": false, "targets": 0 }
-    ]
+    order: app.resultViewModel.state_order(),
+    columnDefs: [{ orderable: false, targets: 0 }],
+    dom:
+      "p" +
+      "<'row'<'col-sm-12 mt-3 results-table-info'i>>" +
+      "t" +
+      "<'#results_table_paginate_bottom.my-3 results_table_paginate'p>",
   });
 
   // $('#pagination-container').append( $('#results_table_paginate') );
-  $('#page-controls').append( $('#results_table_paginate') );
-  $('#results_table_filter').detach();
-  $('#results_table_length').append( $('#results_table_info') );
-  $('#results-controls').append( $('#results_table_length') );
-  $('#results_table_length > label > select').change(change_items_per_page);
+  $("#page-controls").append($("#results_table_paginate"));
+  $("#results_table_filter").detach();
+  $("#results-controls").append($("#results_table_length"));
+  $("#results_table_length > label > select").change(change_items_per_page);
   $("#name_col").click(change_sort);
   $("#category_col").click(change_sort);
   $("#description_col").click(change_sort);
 
-
   //Set up initial state triggers
   reset_triggers();
-} );
+});
