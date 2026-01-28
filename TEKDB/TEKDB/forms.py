0788466f7d38a654cf387fdb833641a595703cb7
Paige Williams <pwilliams@ecotrust.org>
2026-01-28 11:49:18 -0800

    remove unused ThumbnailFileInput and MultipleFileField

--- TEKDB/TEKDB/forms.py
+++ TEKDB/TEKDB/forms.py
@@ -11,21 +11,6 @@ from .models import (
     PlacesResourceEvents,
     Media,
 )
-from .widgets import ThumbnailFileInput
-
-
-class MultipleFileField(forms.FileField):
-    def __init__(self, *args, **kwargs):
-        kwargs.setdefault("widget", ThumbnailFileInput)
-        super().__init__(*args, **kwargs)
-
-    def clean(self, data, initial=None):
-        single_file_clean = super().clean
-        if isinstance(data, (list, tuple)):
-            result = [single_file_clean(d, initial) for d in data]
-        else:
-            result = [single_file_clean(data, initial)]
-        return result
 
 
 class MediaBulkUploadForm(forms.ModelForm):
