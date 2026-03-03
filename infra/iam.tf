3a8e9ab5a20f797b804340f9f6ae5a54c3023f24
Paige Williams <pwilliams@ecotrust.org>
2026-03-03 13:43:44 -0800

    wip: use ssm to deploy docker

--- infra/iam.tf
+++ infra/iam.tf
@@ -21,6 +21,11 @@ resource "aws_iam_role_policy_attachment" "ecr_read" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
 
+resource "aws_iam_role_policy_attachment" "ssm_core" {
+  role       = aws_iam_role.ec2_ecr_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
 # Instance profile wraps the role so EC2 can use it
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "${var.project_name}-ec2-profile"
