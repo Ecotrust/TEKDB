e5eab79e5845dca9265131219d43afc058e3ecce
Paige Williams <pwilliams@ecotrust.org>
2026-03-10 16:48:47 -0700

    test commit

--- TEKDB/TEKDB/wsgi.py
+++ TEKDB/TEKDB/wsgi.py
@@ -15,4 +15,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TEKDB.settings")
 
 application = get_wsgi_application()
+print("test")
 # application = DjangoWhiteNoise(application)
