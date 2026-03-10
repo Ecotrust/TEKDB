17a3ffb4e7597723ebb66ba1a26143b2fc43f6f2
Paige Williams <pwilliams@ecotrust.org>
2026-03-10 13:33:35 -0700

    add CELERY_RESULT_EXTENDED setting

--- TEKDB/TEKDB/settings.py
+++ TEKDB/TEKDB/settings.py
@@ -361,6 +361,7 @@ CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
 CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL", "redis://redis:6379/0")
 CELERY_RESULT_BACKEND = "django-db"
+CELERY_RESULT_EXTENDED = True
 CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
 CELERY_BEAT_SCHEDULE = {
     "delete-expired-chunks-every-48-hours": {
