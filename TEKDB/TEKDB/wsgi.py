a890d527ce4f694853fe6223747f4ddde7e47025
Paige Williams <pwilliams@ecotrust.org>
2026-03-10 17:01:59 -0700

    remove test print statement

--- TEKDB/TEKDB/wsgi.py
+++ TEKDB/TEKDB/wsgi.py
@@ -15,5 +15,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TEKDB.settings")
 
 application = get_wsgi_application()
-print("test again again")
 # application = DjangoWhiteNoise(application)
