f59c1bda2999a0100715af9ca1c6e08343c48c94
Paige Williams <pwilliams@ecotrust.org>
2026-04-20 10:12:16 -0700

    remove unused login urls

--- TEKDB/login/urls.py
+++ TEKDB/login/urls.py
@@ -3,8 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("create", views.create, name="create"),
-    path("forgot", views.forgot, name="forgot"),
     path("", views.index, name="index"),
 ]
-# url(r'^logout$', views.logout, name='logout'),
