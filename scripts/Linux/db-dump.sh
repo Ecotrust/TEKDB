9cb57dfe2965159cb979d46b2b8bd75856954bc0
Paige Williams <pwilliams@ecotrust.org>
2026-06-25 15:30:09 -0700

    add flags to pg_dump

--- scripts/Linux/db-dump.sh
+++ scripts/Linux/db-dump.sh
@@ -21,5 +21,5 @@ BACKUP_FILE="${BACKUP_DIR}/${SQL_DATABASE}.sql"
 mkdir -p "${BACKUP_DIR}"
 
 echo "Dumping database '${SQL_DATABASE}' from container '${CONTAINER_NAME}' to '${BACKUP_FILE}'..."
-docker exec -i "${CONTAINER_NAME}" pg_dump -U "${SQL_USER}" "${SQL_DATABASE}" > "${BACKUP_FILE}"
+docker exec -i "${CONTAINER_NAME}" pg_dump -b -c -n public -O --quote-all-identifiers --no-acl -w -U "${SQL_USER}" "${SQL_DATABASE}" > "${BACKUP_FILE}"
 echo "Database dump completed successfully."
